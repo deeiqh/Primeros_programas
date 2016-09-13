95961b28e738152eafefc27df28ce8a983f21ddd
k-st <kast46.15@gmail.com>
2016-09-13 02:06:54 -0500

    Laboratorio del lunes 12

--- laboratorio.c
+++ laboratorio.c
@@ -3,7 +3,9 @@
 
 void convertir(char*);
 int strindex(char [], char);
-float numero(char*);
+double numero(char*);
+double convierte(char* , char *);
+void invierte(char*, int, int);
 
 int main()
 {
@@ -14,16 +16,11 @@ int main()
     char cad[100] = "Ciencia de la computacioon";
     printf("%d\n", strindex(cad,'o'));
     //numero
-     printf("%f\n", numero("123.56e-6"));
+    printf("%lf\n", numero("123.56e-6"));
     //string invertido
-    char cadena[5];
-    cad[0]='h';
-    cad[1]='o';
-    cad[2]='l';
-    cad[3]='a';
-    cad[4]='\0';
-    invierte(cadena,0,3);
-    printf("%s",cadena );
+    char cadena[] = "holla";
+    invierte(cadena,0,4);
+    printf("%s\n", cadena);
 
     return 0;
 }
@@ -50,67 +47,68 @@ int strindex(char *str, char c){
         return res;
 }
 
-float convierte(char* ptr_i, char *ptr_f)
+double convierte(char* ptr_i, char *ptr_f)
 {
-    float num =0;
+    double num = 0;
     int div = 0;
     int flag = 0;
-    char *ptr = ptr_i;
-    while (ptr_i != ptr_f){
-        if(*ptr != '.'){
-            num = (num*10) + *ptr;
+    while (ptr_i <= ptr_f){
+        if(*ptr_i != '.'){
+            num = (num*10) + (*ptr_i)-'0';
             if (flag == 1)
                 div++;
-            ptr++;
+            ptr_i++;
         }
-        else
+        else{
             flag = 1;
-            ptr++;
+            ptr_i++;
+        }
+    }
     int i;
     for (i=1; i<=div;i++)
         num = num/10;
     return num;
-    }
 }
 
-float numero(char*cadena)
+double numero(char*cadena)
 {
     char *ini_base;
     char *fin_base;
     ini_base = cadena;
-    while (*cadena != 'e')
+    while (*cadena != 'e'){
         fin_base = cadena;
         cadena++;
+    }
     int signo;
+    cadena++;
     signo = (*cadena == '-');
     cadena++;
     char *ini_exp;
     char *fin_exp;
     ini_exp = cadena;
-    while (*cadena != '\0')
+    while (*cadena != '\0'){
         fin_exp = cadena;
         cadena++;
-    float base = convierte(ini_base, fin_base);
-    float exp = convierte(ini_exp, fin_exp);
-
-    return pow(base,exp);
-}
-
-void invierte(char *str,int ini, int fin){
-    if (*str != '\0'){
-        char temp=*str;
-        *str = *(str+fin-ini);
-        *(str+fin-ini) = temp;
-        invierte(str, ini+1,fin-1);
+    }
+    double base = convierte(ini_base, fin_base);
+    double exp = convierte(ini_exp, fin_exp);
+    
+    if (signo == 1){
+        exp = pow(10, -exp);
+        return base * exp;
+    }
+    else{
+        exp = pow(10, exp);
+        return base * exp;
     }
 }
 
-
-
-
-
-
-
-
-
+void invierte(char *str,int ini, int fin){
+    if (fin > 0){
+        char temp = *str;
+        *str = *(str + fin - ini);
+        *(str + fin - ini) = temp;
+        invierte(str + 1, ini, fin-2);
+    }
+}
 
